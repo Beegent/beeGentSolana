ee8ccd01ae2d5ed31e60c1902e32c60c977713a4
jatapsilver <jatapsilver92@hotmail.com>
2026-05-08 18:35:57 -0500

    feat: add agents module with controller and service, integrate TypeORM and JWT
    - Added AgentsModule with AgentsController and AgentsService.
    - Integrated @nestjs/config for environment variable management.
    - Configured TypeORM with PostgreSQL connection settings.
    - Implemented JWT authentication with configurable secret.
    - Added global validation pipe and logger middleware.
    - Set up Swagger for API documentation.
    - Created example .env file for database configuration.
    - Defined Angets entity for database interactions.

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AgentsModule } from './agents/agents.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('typeorm')!,
+    }),
+    AgentsModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
